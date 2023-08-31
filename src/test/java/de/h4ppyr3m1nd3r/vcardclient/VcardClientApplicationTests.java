29f62d9e9831c83ad14d155920505c1a57e6af68
Alexander Friedrichs <undso@users.noreply.github.com>
2023-08-31 22:48:53 +0200

    spring boot initializr

---
+++ src/test/java/de/h4ppyr3m1nd3r/vcardclient/VcardClientApplicationTests.java
@@ -0,0 +1,13 @@
+package de.h4ppyr3m1nd3r.vcardclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VcardClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
